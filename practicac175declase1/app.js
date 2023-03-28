68f6a4cbe8708269f349135f41685bc7651c8b11
diana vazquez <diasolva@gmail.com>
2023-03-28 20:35:22 -0300

    prmer commit

---
+++ practicac175declase1/app.js
@@ -0,0 +1,33 @@
+//ejercicio 1
+var auto ="Corolla"
+console.log (auto)
+
+//ejercicio 2
+auto = 308
+console.log(auto)
+
+//ejercicio 3
+auto = "C4" 
+console.log(auto)
+
+//ejercicio 4
+var nombre = "Diana"
+var apellido = "Vazquez"
+console.log(nombre , apellido)
+
+//ejercicio 5
+var fruta = "naranja"
+var tipoDeDatoDeLaVariableFruta = typeof(fruta)
+console.log(tipoDeDatoDeLaVariableFruta)
+
+//ejercicio 6 
+var tipoDeDatoDeLaVariableAuto = typeof(auto)
+console.log(tipoDeDatoDeLaVariableAuto)
+
+var auto = 308
+var tipoDeDatoDeLaVariableAuto = typeof(auto)
+console.log(tipoDeDatoDeLaVariableAuto)
+
+var auto = "C4"
+var tipoDeDatoDeLaVariableAuto = typeof(auto)
+console.log(tipoDeDatoDeLaVariableAuto)
